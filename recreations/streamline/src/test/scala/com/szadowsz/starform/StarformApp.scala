49897cfedd1bb0edfcf5d224596877339837abff
Patrick Gremo <patrick.gremo@yum.com>
2022-09-06 08:51:43 -0500

    change log formatter

--- recreations/streamline/src/test/scala/com/szadowsz/starform/StarformApp.scala
+++ recreations/streamline/src/test/scala/com/szadowsz/starform/StarformApp.scala
@@ -2,8 +2,6 @@ package com.szadowsz.starform
 
 import com.szadowsz.starform.model.SimConstants
 
-import java.lang.System.Logger.Level.INFO
-
 /**
   * A Modification of DoleAccretionTestCase. Simple Application to generate and print off planets to the logs.
   *
@@ -14,8 +12,9 @@ object StarformApp {
   protected val logger: System.Logger = System.getLogger(getClass.getName)
 
   def main(args: Array[String]): Unit = {
+    System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %3$s %5$s%6$s%n")
     val starform = new StarformSimulation(SimConstants(None, None, None, None, None))
     val system = starform.generateSystem(Some(1))
-    system.planets.zipWithIndex.foreach { case (pl, i) => logger.log(INFO, "Planet {0}: {1}", i, pl) }
+    system.planets.zipWithIndex.foreach { case (pl, i) => printf("Planet %s: %s%n", i, pl) }
   }
 }
