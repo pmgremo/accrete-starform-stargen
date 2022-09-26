634e8a2a7f365ec8f492b48ade41b5b43ad7e098
Patrick Gremo <patrick.gremo@yum.com>
2022-09-26 09:21:14 -0500

    simplify nix shell config

--- shell.nix
+++ shell.nix
@@ -1,13 +1,12 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21de2b973f9fee595a7a1ac4693efff791245c34.tar.gz") {}
-}:let
-
-in pkgs.mkShell {
-
+let
+    pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21de2b973f9fee595a7a1ac4693efff791245c34.tar.gz"){};
+in
+pkgs.mkShell {
   buildInputs = with pkgs; [
-    git
-    jdk17_headless
-    gradle
-    gcc
     dotty
+    gcc
+    git
+    gradle
+    jdk17_headless
   ];
 }
