9e38f48c2b5ceab658dd5908d757016d3e3e3344
Patrick Gremo <patrick.gremo@yum.com>
2022-09-26 09:25:20 -0500

    check if logger is loggable before logging

--- recreations/streamline/src/main/scala/com/szadowsz/starform/log/Log.scala
+++ recreations/streamline/src/main/scala/com/szadowsz/starform/log/Log.scala
@@ -7,24 +7,21 @@ import java.lang.System.Logger.Level.*
 
 trait Log {
   protected[this] lazy val logger: Logger = System.getLogger(this.getClass.getName)
-
-  inline def error(inline message: Any): Unit = ${ LoggerMacros.logImpl('logger, 'ERROR,'message) }
-
-  inline def warn(inline message: Any): Unit = ${ LoggerMacros.logImpl('logger, 'WARNING, 'message) }
-
-  inline def info(inline message: Any): Unit = ${ LoggerMacros.logImpl('logger, 'INFO, 'message) }
-
-  inline def debug(inline message: Any): Unit = ${ LoggerMacros.logImpl('logger, 'DEBUG, 'message) }
-
-  inline def trace(inline message: Any): Unit = ${ LoggerMacros.logImpl('logger, 'TRACE, 'message) }
+  inline def error(inline message: Any): Unit = ${ Log.logImpl('logger, 'ERROR,'message) }
+  inline def warn(inline message: Any): Unit = ${ Log.logImpl('logger, 'WARNING, 'message) }
+  inline def info(inline message: Any): Unit = ${ Log.logImpl('logger, 'INFO, 'message) }
+  inline def debug(inline message: Any): Unit = ${ Log.logImpl('logger, 'DEBUG, 'message) }
+  inline def trace(inline message: Any): Unit = ${ Log.logImpl('logger, 'TRACE, 'message) }
 }
 
-private[log] object LoggerMacros {
+private[log] object Log {
   import scala.quoted._
 
   def logImpl(logger: Expr[Logger], logLevel: Expr[Level], message: Expr[Any])(using q: Quotes): Expr[Unit] = {
     '{
-      ${ logger }.log(${ logLevel }, ${ message })
+      if (${ logger }.isLoggable(${ logLevel })) {
+        ${ logger }.log(${ logLevel }, ${ message })
+      }
     }
   }
 }
